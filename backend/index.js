2d7e084cfab2d422b3fdd64d6dd698a0dac96a32
ramyabyyu <bintantramy@gmail.com>
2022-08-09 14:30:33 +0700

    Ready to Deploy to Heroku

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const colors = require("colors");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
 const { errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path");
 
 // Database Connection
 connectDB();
@@ -16,6 +17,19 @@ app.use(errorHandler);
 
 // Routes
 app.use("/api", require("./routes"));
+
+// Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("You're not in production"));
+}
 
 const port = process.env.PORT || 8080;
 app.listen(port, () =>
